cd3ea2c99467f656068a678a8e14e798ce66f0f1
Gabriel Padolina <gabriel.padolina@petnet.com.ph>
2024-07-16 11:09:21 +0800

    added getting all products in controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index(){
-        return 'asdsadsa';
+        $product = Product::all();
+
+        $data = [
+            'status'=>200,
+            'product'=>$product
+        ];
+
+        return response()->json($data,200);
     }
 }
